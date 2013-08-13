2ce45cae9229b5c1f1088e851dc31ab809f41343
schultyy <janschulte90@googlemail.com>
2013-08-13 18:36:46 +0200

    Removed index.html. Can show window

---
+++ js/background.js
@@ -0,0 +1,16 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+  // Center window on screen.
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+  var width = 900;
+  var height = 600;
+
+  chrome.app.window.create('../window.html', {
+    bounds: {
+      width: width,
+      height: height,
+      left: Math.round((screenWidth-width)/2),
+      top: Math.round((screenHeight-height)/2)
+    }
+  });
+});
