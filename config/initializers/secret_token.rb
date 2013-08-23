888c2147587c06532a5c3469505ad73cc55a175e
schultyy <janschulte90@googlemail.com>
2013-08-23 16:46:48 +0200

    moved js files to app/assets/js

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Empfaenger::Application.config.secret_key_base = 'b3951ab5b1cf6361a23c19f013cad90f9e1183ad32c5e3a69e373a77b030954862cb6eb0fdd426207e1f9f8623cdd2a2f2741ca13f8703220b4581d04364b0c1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Empfaenger::Application.config.session_store :cookie_store, key: '_empfaenger_session'
