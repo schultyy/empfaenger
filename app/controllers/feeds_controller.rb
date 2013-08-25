bc37574639bc6b5f8a79303b3132b9312a72eb02
schultyy <janschulte90@googlemail.com>
2013-08-25 15:26:02 +0200

    Passes Feed spec

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -3,5 +3,15 @@ class FeedsController < ApplicationController
 
   end
   def create
+    feed = Feed.new(feed_params)
+    if feed.save
+      redirect_to "/"
+     else
+      render nothing: true
+    end
+  end
+  private
+  def feed_params
+    params.require(:feed).permit(:address)
   end
 end
--- spec/controllers/feeds_controller_spec.rb
+++ spec/controllers/feeds_controller_spec.rb
@@ -24,5 +24,4 @@ describe FeedsController do
       response.should render_template :new
     end
   end
-
 end
